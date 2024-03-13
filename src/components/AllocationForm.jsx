fbe2b6ee1cf09beead61d8a2ea2b31f5743f4e06
ElBookyes <dtuparov94@gmail.com>
2024-03-13 17:57:02 +0200

    HR fix

--- src/components/AllocationForm.jsx
+++ src/components/AllocationForm.jsx
@@ -57,14 +57,11 @@ const AllocationForm = (props) => {
             <option value="Finance" name="finance">
               Finance
             </option>
-            <option value="HR" name="hr">
+            <option value="Human Resource" name="Human Resource">
               HR
             </option>
             <option value="IT" name="it">
               IT
-            </option>
-            <option value="Admin" name="admin">
-              Admin
             </option>
           </select>
 
